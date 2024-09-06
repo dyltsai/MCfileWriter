e3d62161b6308e8345d82725cbd55ffc3dcb48c1
Aviv Pilipski <apilipski1@hwemail.com>
2024-09-05 17:22:28 -0700

    BufferedWriter Hidden/Non-Hidden Files
    Used the BufferedWriter class to write text to a hidden file called .notTheissBankAccount.txt and also wrote text to a non-hidden file called honorCode.hw within a hidden directory called .notClassified

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -1,50 +1,23 @@
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
 
 public class MyFileWriter {
-    public static void main(String[] args) {
-        String data = "Hello, World!";
-        String fileName1 = "example1.txt";
-        String fileName2 = "example2.txt";
-        String fileName3 = "example3.txt";
-        String fileName4 = "example4.txt";
-        String fileName5 = "example5.txt";
-
-        // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        String password = "Sorry I Couldn't Find Your Bank Account Info, I Tried Mr. Theiss :()";
+        String topSecretData = "I have neither given nor recieved unauthorized aid on this project. My name affirms my honor";
 
         // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
-            bufferedWriter.write(data);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(".notTheissBankAccount.txt"))) {
+            bufferedWriter.write(password);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(".notClassified/honorCode.hw"))) {
+            bufferedWriter.write(topSecretData);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
-            bufferedOutputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 5. Using Files (java.nio.file)
-        try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }

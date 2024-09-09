8bd665294932ce1e6b448b3280c7317243003428
avivpilipski <apilipski1@hwemail.com>
2024-09-09 13:27:15 -0700

    Implemented getFileSize Method (#1)
    * Implemented printFileSize method
    
    Takes in String fileName and prints it's size!
    
    * Fixed printFileSize Method and Tested It

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -2,6 +2,12 @@
 import java.io.*;
 
 public class MyFileWriter {
+   
+    public static void printFileSize (String fileName) {
+        File file = new File (fileName);
+        System.out.println(file.length());
+    }
+
     public static void main(String[] args) throws IOException {
         String password = "Sorry I Couldn't Find Your Bank Account Info, I Tried Mr. Theiss :()";
         String topSecretData = "I have neither given nor recieved unauthorized aid on this project. My name affirms my honor";
@@ -18,6 +24,7 @@ public class MyFileWriter {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        printFileSize(".notTheissBankAccount.txt");
 
     }
 }
